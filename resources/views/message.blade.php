02ff1d3b0eb78b464a11488a65a68761300eb9e8
lbognar <bognar.lorand@gmail.com>
2021-04-14 18:48:40 +0300

    --  only catch messages meant to be displayed as alerts

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,10 +1,12 @@
 <div class="flash-messages">
     @if(isset(session()->all()['_flash']))
         @foreach (session()->all()['_flash']['old'] as $flashKey)
+            @if(is_string (session()->get($flashKey . '.content')))
             <p class="alert alert-{{ session()->get($flashKey . '.type', 'default') }} mt-3">
                 {{ session()->get($flashKey.'.content') ?? session()->get($flashKey) }}
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
             </p>
+            @endif
         @endforeach
     @endif
 </div> <!-- end .flash-message -->
