6533151008ebfc8de551e258f130fe44dabbd059
lbognar <bognar.lorand@gmail.com>
2021-04-18 04:24:29 +0300

    42 - eager loading
    we optimized so instead of loading the companies in separate query for every user, now we only query for companies once.

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -20,7 +20,7 @@ class CustomersController extends Controller
 
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::with('company')->get();
 
         return view('customers.index', compact('customers'));
     }
